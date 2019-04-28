5753c405e8789a5092e4a77af966ea39834f541f
c00ldude1oo <crluke11@gmail.com>
2019-04-28 17:31:11 -0400

    Update OSbuild.bat

--- Files/OSbuild.bat
+++ Files/OSbuild.bat
@@ -1,1 +1,2 @@
+REM created by @c00ldude1oo
 @echo off && if not "%1" == "/?" ( systeminfo | findstr /B/C:"OS Version" > %tmp%\file.tmp && for /F "tokens=1,5,6" %%i IN (%tmp%\file.tmp) DO set OSb= %%i %%j %%k && DEL %tmp%\file.tmp && echo %OSb% %1 ) else ( echo. && echo HELP: Prints OS Build number and saves it to %%OSb%%. )
